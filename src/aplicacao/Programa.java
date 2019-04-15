c293b4169805f8cd4386ffb4b5fafa2cd698d89d
André Luiz Morita <andreluizmorita@gmail.com>
2019-04-15 20:04:51 -0300

    Finish tests

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -10,7 +10,36 @@ public class Programa {
 
 	public static void main(String[] args) {
 	
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
 		
+		Pessoa p = em.find(Pessoa.class, 2);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		System.out.println(p);
+		
+		System.out.println("Pronto!");
+		em.close();
+		emf.close();
+	}
+	
+	public void remove() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		Pessoa p = em.find(Pessoa.class, 2);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		System.out.println(p);
+		
+		System.out.println("Pronto!");
+		em.close();
+		emf.close();
+	}
+	
+	public void find() {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
